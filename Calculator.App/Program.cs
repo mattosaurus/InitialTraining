0086b78b0cd080a04444572de631f37db77996b5
mattosaurus <bitscry@outlook.com>
2021-11-24 12:51:52 +0000

    Update framework

---
+++ Calculator.App/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace InitialTraining.Calculator.App
+{
+    static class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
