4e0e0b842e5e346af2008348887fa0916af4ff75
mattosaurus <bitscry@outlook.com>
2021-12-14 17:25:49 +0000

    Recreat app

---
+++ Calculator.App/Program.cs
@@ -0,0 +1,10 @@
+namespace InitialTraining.Calculator.App
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
